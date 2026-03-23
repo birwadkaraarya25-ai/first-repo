ac2e948ed1eb9fb812f6d99319b250f846b1721f
birwadkaraarya25-ai <birwadkaraarya25@gmail.com>
2026-03-23 23:38:24 +0530

    assignment 18.c
    Removed comments and improved string handling.

--- assignment 18.c
+++ assignment 18.c
@@ -6,21 +6,19 @@ void main()
     char str1[100], str2[100], rev[100];
     int i, length = 0, flag = 1;
 
-    // Input strings
     printf("Enter a string: ");
     scanf("%s", str1);
 
     printf("Enter another string: ");
     scanf("%s", str2);
 
-    // Find length
     while (str1[length] != '\0')
     {
         length++;
     }
     printf("\nLength of string = %d", length);
 
-    // Reverse string
+
     for (i = 0; i < length; i++)
     {
         rev[i] = str1[length - i - 1];
@@ -29,7 +27,7 @@ void main()
 
     printf("\nReversed string = %s", rev);
 
-    // Palindrome check
+    
     for (i = 0; i < length; i++)
     {
         if (str1[i] != rev[i])
@@ -44,13 +42,12 @@ void main()
     else
         printf("\nThe string is NOT a Palindrome");
 
-    // Equality check
+
     if (strcmp(str1, str2) == 0)
         printf("\nStrings are Equal");
     else
         printf("\nStrings are NOT Equal");
 
-    // Substring check
     if (strstr(str1, str2) != NULL)
         printf("\nSubstring found in main string");
     else
